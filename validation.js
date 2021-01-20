18a29349ef0d0eea2ce3d4b0474129a1eecf0e20
Anthony Rizzo <arizzo0715@gmail.com>
2021-01-20 18:20:02 -0500

    Get, update, delete users

--- validation.js
+++ validation.js
@@ -7,9 +7,9 @@ const Joi = require('joi');
 //Validate user registration
 const registerValidation = userData => {
     const userSchema = Joi.object({
-        name: Joi.string().min(6).max(255).required(),
-        email: Joi.string().min(6).required().email(),
-        password: Joi.string().min(6).required()
+        name: Joi.string().min(6).max(255),
+        email: Joi.string().min(6).email(),
+        password: Joi.string().min(6)
     });
 
     return userSchema.validate(userData);
